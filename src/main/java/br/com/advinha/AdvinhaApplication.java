8e1b2dc98ccf0d99b6f98915209576d106f18124
pereiracaslu <lucascosta2512@gmail.com>
2022-09-17 11:17:47 -0300

    Iniciando repositório

---
+++ src/main/java/br/com/advinha/AdvinhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.advinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvinhaApplication.class, args);
+	}
+
+}
